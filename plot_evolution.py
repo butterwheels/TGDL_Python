3e2e8a291b2f331f14756896dbd95443ed180557
j.denholm.2017@gmail.com <j.denholm.2017@gmail.com>
2019-07-05 14:51:30 +0100

    messing around with plotting output

--- plot_evolution.py
+++ plot_evolution.py
@@ -1,30 +1,42 @@
 #!/usr/bin/env python3
 """Code ot plot thew TGDL solutions."""
+import os
 import numpy as np
 import matplotlib.pyplot as plt
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
 
 # Path to the location you want to make a movie of
 load_path = '/home/james/Ising_Model_Codes/TGDL_Solutions/'
-file_name = 'diagonal.npy'
+file_names = ["ground.npy", "on_axis.npy", "diagonal.npy"]
 
-snapshots = np.load(load_path + file_name)
-sample_times = np.load(load_path + 'measured_times.npy')
+frame_nums = [225, 250, 400, 500]
 
-frame_nums = [200, 250, 400, 400]
+width = 3.375
+height = 2.75
 
-width = 6
-height = 2
+fig, ax = plt.subplots(3, len(frame_nums), figsize=(width, height))
+fig.subplots_adjust(left=0.01, right=0.99, bottom=0.075, top=0.99,
+                    wspace=0.05, hspace=0.1)
 
-fig, ax = plt.subplots(1, len(frame_nums), figsize=(width, height))
+for file_name in enumerate(file_names):
 
-for i in range(len(frame_nums)):
+    snapshots = np.load(load_path + file_name[1])
+    sample_times = np.load(load_path + 'measured_times.npy')
 
-    frame_num = frame_nums[i]
-    print(sample_times[i])
+    for i in range(len(frame_nums)):
 
-    ax[i].imshow(snapshots[:, :, frame_num], vmin=-1, vmax=1, cmap="RdBu")
+        frame_num = frame_nums[i]
 
-    ax[i].set_xticks([])
-    ax[i].set_yticks([])
+        ax[file_name[0], i].imshow(snapshots[:, :, frame_num],
+                                   vmin=-1, vmax=1, cmap="YlGnBu")
 
-plt.show()
+        ax[file_name[0], i].set_xticks([])
+        ax[file_name[0], i].set_yticks([])
+
+        ax[-1, i].set_xlabel(r"$t = %0.4f$" % sample_times[i])
+
+if not os.path.exists("images/"):
+    os.makedirs("images/")
+
+fig.savefig("images/tgdl_evo.eps", format="eps", dpi=1000)
